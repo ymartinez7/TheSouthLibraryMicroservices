21cd2865741b1aaf6896ccaef5becca515749d38
Yansel Martinez <ymartinez7@gmail.com>
2022-03-23 21:58:20 +0100

    Prueba ejecución CI/CD con creación del cluster AKS

--- src/ApiGateway/TheSouthlLibrary.Microservices.Gateway.Api/Controllers/WeatherForecastController.cs
+++ src/ApiGateway/TheSouthlLibrary.Microservices.Gateway.Api/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace TheSouthlLibrary.Gateway.Api.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
